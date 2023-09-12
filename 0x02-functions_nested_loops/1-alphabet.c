21bf05b600e7932b7a8b09062b13be24281ef61a
juancyrus <mragbadinoahkekeli@gmail.com>
2023-09-12 10:32:43 -0700

    soffrn

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include <main.h>
+/** 
+ * Prints alphalbets unto new line 
+*/
+void print_alphabet(void)
+{
+	char ch;
+	for (ch ='a' ; ch <= 'z'; ch++)
+{
+	_putchar(ch);
+}
+        _putchar(ch);
+	}
