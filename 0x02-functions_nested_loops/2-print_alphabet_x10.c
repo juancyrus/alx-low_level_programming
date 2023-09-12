bb777a7b564d5b880d2f85b2a8d4bbc875982efa
juancyrus <mragbadinoahkekeli@gmail.com>
2023-09-12 12:47:13 -0700

    soffrn

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,9 +7,9 @@ void print_alphabet_x10(void)
 	char ch;
 	int count;
 
-        for (count = 0 , count < 10 , count ++)
+        for (count = 0 ;count < 10 ;count ++)
 	{
-		for (ch = 'a' , ch <= 'z' , ch++)
+		for (ch = 'a' ;ch <= 'z' ;ch++)
 	{
 			_putchar(ch);
 	}
