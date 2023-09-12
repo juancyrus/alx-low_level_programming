23836bece8c30a8e72c4bf31b8a2bf3e680b2915
juancyrus <mragbadinoahkekeli@gmail.com>
2023-09-12 12:31:38 -0700

    call me

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main"
+/**
+ *print_alphabet_x10 -Prints the alphabet 10 times in lowercase, followed by a new line.
+ */
+void print_alphabet_x10(void);
+{
+	char ch;
+	int count;
+
+        for (count = 0 , count < 10 , count ++)
+	{
+		for (ch = 'a' , ch <= 'z' , ch = ch++)
+	{
+			_putchar(ch);
+	}
+		_putchar('\n');
+	}
+}
