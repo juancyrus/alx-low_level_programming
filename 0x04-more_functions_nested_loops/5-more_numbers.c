efdda27783ef20a7bffbccec77c5b5b0835a9477
juancyrus <mragbadinoahkekeli@gmail.com>
2023-09-14 16:00:01 -0700

    soffrn

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,11 +4,15 @@
 *
 *Return: returns nothing
 */
-void more_numbers(void) {
+void more_numbers(void) 
+{
 int i, num;
-for (i = 0; i < 10; i++) {
-for (num = 0; num <= 14; num++) {
-if (num > 9) {
+for (i = 0; i < 10; i++) 
+{
+for (num = 0; num <= 14; num++)
+{
+if (num > 9)
+{
 _putchar(num / 10 + '0');
 }
 _putchar(num % 10 + '0');
