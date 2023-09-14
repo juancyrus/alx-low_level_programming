842089a85b6de45dc594cf7b3620c5d8e1f0a8de
juancyrus <mragbadinoahkekeli@gmail.com>
2023-09-14 15:49:05 -0700

    soffrn

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*more_numbers - prints numbers to 14
+*
+*Return: returns nothing
+*/
+void more_numbers(void) {
+int i, num;
+for (i = 0; i < 10; i++) {
+for (num = 0; num <= 14; num++) {
+if (num > 9) {
+_putchar(num / 10 + '0');
+}
+_putchar(num % 10 + '0');
+}
+_putchar('\n');
+}
+}
