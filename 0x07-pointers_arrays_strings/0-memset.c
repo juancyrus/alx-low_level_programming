5ad90cd00bebeb5a3328987feca79ebcd00e81b7
juancyrus <mragbadinoahkekeli@gmail.com>
2023-09-25 15:15:51 -0700

    fok

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main"
+/**
+ * _memset - This function fills
+ * the first n bytes of the memory area
+ * pointed to b s with the constant byte b
+ * @s: target
+ * @b: constant byte
+ * @n: number of byte
+ * Return : returns new value of target 
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+while (n)
+{
+s[n -1] = b;
+n--;
+}
+return (s);
+}
