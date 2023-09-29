bd45b5ffc0cf38e334ce5270c4655a5427a057a6
juancyrus <mragbadinoahkekeli@gmail.com>
2023-09-29 15:43:40 -0700

    sdofn

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
+
 /**
  * function that prints its own name 
  * main - prints program name
 *@argc: number of arguments
 *@argv: array of arguments
 *Return: 0
+*
 */
+
 int main(int argc, char *argv[])
 {
-	printf("%s\n', argv[0]);
+printf("%s\n", argv[0]);
 
-	return (0);
+return (0);
+
 }
 
