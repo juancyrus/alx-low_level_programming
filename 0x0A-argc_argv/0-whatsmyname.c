6fb8a497f5ce735bc2a1129e7fe8b8093d77e704
juancyrus <mragbadinoahkekeli@gmail.com>
2023-09-29 15:33:41 -0700

    soffrn

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * function that prints its own name 
+ * main - prints program name
+*@argc: number of arguments
+*@argv: array of arguments
+*Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	printf("%s\n', argv[0]);
+
+	return (0);
+}
+
