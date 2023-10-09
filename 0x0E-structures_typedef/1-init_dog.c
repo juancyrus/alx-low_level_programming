35fd341528b38eddb24b04d04fd542bc1362f8b2
juancyrus <mragbadinoahkekeli@gmail.com>
2023-10-09 22:43:21 +0000

    soffrn

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+/**
+ * init_dog- will initiate a variable of type struct dog
+ * for d - indentification'
+ * name - dog's title
+ * age - revs around sun
+ * owner - name of human parents
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d!=NULL)
+{
+(*d).name = name;
+(*d).age = age;
+(*d).owner = owner;
+}
+}
