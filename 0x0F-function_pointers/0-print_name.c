e9687deeb9881133c37d37314e78a5dada0a7c95
juancyrus <mragbadinoahkekeli@gmail.com>
2023-10-12 20:59:08 +0000

    soffrn

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,17 @@
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name.
- * @name: input name.
- * @f: function pointer.
- *
- * Return: no return.
- */
+*print_name - prints a name
+*@name:name to print
+*@f: function which print name
+*/
+
 void print_name(char *name, void (*f)(char *))
 {
-if (name && f)
+
+if (name == NULL || f == NULL)
+return;
+
 f(name);
+
 }
